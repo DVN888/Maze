6a69a68a461a53d1ec93e773ae28d7f3c07d4ad9
DVN888 <dungvietnguyen888@gmail.com>
2026-08-15 16:27:03 +0200

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -88,47 +88,6 @@ public class Graph {
         }
     }
 
-    /**  
-     * Initializes a graph from the specified input stream.
-     * The format is the number of vertices <em>V</em>,
-     * followed by the number of edges <em>E</em>,
-     * followed by <em>E</em> pairs of vertices, with each entry separated by whitespace.
-     *
-     * @param  in the input stream
-     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
-     * @throws IllegalArgumentException if the number of vertices or edges is negative
-     * @throws IllegalArgumentException if the input stream is in the wrong format
-     */
-    public Graph(In in) {
-        try {
-            this.V = in.readInt();
-            if (V < 0) throw new IllegalArgumentException("number of vertices in a Graph must be nonnegative");
-            adj = new LinkedList[V];
-            for (int v = 0; v < V; v++) {
-                adj[v] = new LinkedList<Integer>();
-            }
-            int E = in.readInt();
-            if (E < 0) throw new IllegalArgumentException("number of edges in a Graph must be nonnegative");
-            in.readLine();
-            int v=0;
-            while(in.hasNextLine()) {
-            	validateVertex(v);
-            	String line= in.readLine();
-            	String [] adjacent= line.split(" ");
-            	for (String n: adjacent) {
-            		int w= Integer.parseInt(n);
-            		validateVertex(w);
-            		addEdge(v, w);
-            	}
-            	v=v+1;
-            }
-        }
-        catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("invalid input format in Graph constructor", e);
-        }
-    }
-
-
     /**
      * Initializes a new graph that is a deep copy of {@code G}.
      *
