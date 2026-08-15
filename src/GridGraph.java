597091f0d7f6ec25db19e25652b56b8e723bd6f0
DVN888 <dungvietnguyen888@gmail.com>
2026-08-15 16:28:24 +0200

    Update GridGraph.java

--- src/GridGraph.java
+++ src/GridGraph.java
@@ -102,53 +102,5 @@ public class GridGraph extends Visualization{
 
 	    }
 	  }
-
-	public GridGraph(Graph G, List <Integer> path, Color color) {
-		super(G);
-		double grid= canvas;
-		int column= (int) Math.sqrt(nodesl);
-		int gridlength = (int)Math.ceil(grid/column);
-		int k=0;
-		for (int node=0; node< G.V();node++) {
-			int x= node %column;
-			int y= (int) Math.floor(node/column);
-			vertices[k].setLocation(y*gridlength, canvas-x*(gridlength-1));
-			k++;
-		}
-		plot();
-		plot(path, 2, color);
-	}
-
-	public void plot(List<Integer> path, double enlargeStart, Color color)
-	{
-		int index=0;
-		//color nodes and edges in path differently
-		for (int i =0; i<path.size(); i++) {
-			int node=path.get(i);
-			Point2D.Double p0 = vertices[node];
-			StdDraw.setPenColor(color);
-			StdDraw.setPenRadius(config.LINEWIDTH);
-			StdDraw.point(p0.getX(), p0.getY());
-			//StdDraw.point(p1.getX(), p1.getY());
-			//StdDraw.filledCircle(p0.getX(), p0.getY(), config.r*enlargeStart);
-
-			for (int adj : G.adj(node)) {
-				if (i<path.size()-1 && adj==path.get(i+1)) {
-					StdDraw.setPenColor(color);
-					StdDraw.setPenRadius(config.LINEWIDTH_MARKED);
-				}
-				else {
-					StdDraw.setPenColor(config.COL_FG);
-					StdDraw.setPenRadius(config.LINEWIDTH);
-				}
-				Point2D.Double p1 = vertices[adj];
-				StdDraw.line(p0.getX(), p0.getY(), p1.getX(), p1.getY());
-
-			}
-			//StdDraw.pause(100);
-			StdDraw.show();
-
-		}
-	}
 }
 
